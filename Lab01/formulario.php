1c528fe67fe6e01cac25926a5fd73fd4be7b3379
Antuan1 <Jhonatan.diaz@tecsup.edu.pe>
2018-03-12 15:38:11 -0500

    first commit

---
+++ Lab01/formulario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulario php</title>
+</head>
+<body>
+	<form method="POST" action="sumar.php">
+		<p>VALOR 1:<input type="text" name="T1" size="20"></p>
+
+		<p>VALOR 2:<input type="text" name="T2" size="20"></p>
+		<p><input type="submit" value="Sumar" name="B1"></p>
+
+	</form>
+
+</body>
+</html>
