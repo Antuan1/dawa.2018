390f74250a4cab1bbd9ad5c0acf6f139dc684a2b
Jhonatan Diaz Flores <Jhonatan.diaz@tecsup.edu.pe>
2018-03-19 18:16:57 -0500

    Add files via upload

---
+++ Lab02/importar.js
@@ -0,0 +1,29 @@
+var http = require('http');
+	fs = require('fs'),
+	parser = require('./parser_var.js'),
+	datos = parser.batman,
+	p = parser.parse_vars;
+
+
+http.createServer(function(req,res){
+	fs.readFile('./form.html',function(err,html){
+		var html_string = html.toString();
+		 
+		 var respuesta =p(req),
+		 parametros= respuesta['parametros'],
+		 valores = respuesta['valores'];
+
+		
+		for(var i=0;i<parametros.length;i++){
+			html_string = html_string.replace('{'+parametros[i]+'}',valores[i]);
+		}
+
+		html_string =html_string.replace('{identidad}',datos['identidad']);
+		html_string =html_string.replace('{poder}',datos['poder']);
+
+
+		res.writeHead(200,{'Content-type':'text/html'});
+		res.write(html_string);
+		res.end();
+	});
+}).listen(3000);
