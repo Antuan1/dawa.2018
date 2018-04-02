b16166e39afef86ca4d1bca9ed863548a475f4d7
Anthony <Jhonatan.diaz@tecsup.edu.pe>
2018-04-02 16:46:27 -0500

    Laboratorio 4

---
+++ Lab04.1/index.js
@@ -0,0 +1,33 @@
+var express = require('express')
+var app = express()
+app.use('/static',express.static('public'))
+app.use(function(res,req,next){
+	res.status(404).send("Esto no existe")
+})
+
+app.use(function(err,res,req,next){
+	console.error(err.stack)
+	res.status(500).send('Algo salio mal')
+})
+
+
+
+app.get('/',function (req,res){
+	res.send('Hola Mundo! en Express:')
+})
+
+app.post('/',function (req,res){
+	res.send('Llamada Post misma url')
+})
+
+app.put('/user',function (req,res){
+	res.send('Recibimos un put en /User')
+})
+
+app.delete('/User',function (req,res){
+	res.send('Recibimos un DELETE en /user')
+})
+
+app.listen(3000,function(){
+	console.log('Aplicacion de ejemplo ejecutando en el puerto 3000!')
+});
